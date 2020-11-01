11cd861fdfe6da9b89d3e95ef17041459bb6ea1a
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-01 22:57:38 +0100

    init

--- src/service-protocol.js
+++ src/service-protocol.js
@@ -1,77 +1,61 @@
 'use strict';
 
 class ServiceProtocol {
+
   constructor(bravia, protocol) {
     this.bravia = bravia;
     this.protocol = protocol;
     this._methods = [];
   }
 
-  getVersions() {
-    return this.invoke('getVersions');
+  async getVersions() {
+    const versions = await this.invoke('getVersions');
+    return versions;
   }
 
-  getMethodTypes(version) {
-    return new Promise((resolve, reject) => {
-      if (this._methods.length > 0) {
-        if (version) {
-          resolve(this._methods.find(method => method.version === version));
-        } else {
-          resolve(this._methods);
-        }
-        return;
+  async getMethodTypes(version) {
+   
+    if (this._methods.length > 0) {
+      if (version) {
+        return this._methods.find(method => method.version === version);
+      } else {
+        return this._methods;
       }
+    }
+    
+    const versions = await this.getVersions();
+    
+    for(const id of versions){
+      let data = await this.invoke('getMethodTypes', '1.0', id);
+      this._methods.push({ version: id, methods: data });
+    }
+    
+    return this._methods;
 
-      this.getVersions().then(versions => {
-        let index = 0;
-        let next = (results) => {
-          if (results) {
-            this._methods.push({ version: versions[index - 1], methods: results });
-          }
-
-          if (index < versions.length) {
-            this.invoke('getMethodTypes', '1.0', versions[index++]).then(next, reject);
-          } else {
-            if (version) {
-              resolve(this._methods.find(method => method.version === version));
-            } else {
-              resolve(this._methods);
-            }
-          }
-        };
-
-        next();
-      }, reject);
-    });
   }
 
-  invoke(method, version = '1.0', params) {
-    return new Promise((resolve, reject) => {
-      params = params ? [params] : [];
-      this.bravia._request({
-        path: `/${this.protocol}`,
-        json: {
-          id: 3,
-          method: method,
-          version: version,
-          params: params
-        }
-      }).then(response => {
-        if (response.error) {
-          reject(new Error(response.error[1]));
-          return;
-        }
-
-        if (response.results) {
-          resolve(response.results);
-        } else if (response.result) {
-          resolve(response.result[(response.result.length > 1 ? 1 : 0)]);
-        } else {
-          resolve();
-        }
-      }, reject);
+  async invoke(method, version, params) {
+  
+    const response = await this.bravia._request({
+      path: `/${this.protocol}`,
+      json: {
+        id: 3,
+        method: method,
+        version: version || '1.0',
+        params: params ? [params] : []
+      }
     });
+
+    if (response.data.results) {
+      return response.data.results;
+    } else if (response.data.result) {
+      return response.data.result[(response.data.result.length > 1 ? 1 : 0)];
+    }
+     
+    return;
+     
   }
+  
 }
 
 module.exports = ServiceProtocol;
