069317aa1873b6ea4c2c8f4e5cf32581c85fbe6a
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-01 22:06:37 +0100

    init

--- src/bravia.js
+++ src/bravia.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const SsdpClient = require('node-ssdp').Client;
-const Request = require('request');
-const URL = require('url');
+const axios = require('axios');
+const base64 = require('base-64');
 const parseString = require('xml2js').parseString;
+const { prompt } = require('enquirer');  
+const SsdpClient = require('node-ssdp').Client;
+const URL = require('url');
+const wol = require('wake_on_lan'); 
 
 const ServiceProtocol = require('./service-protocol');
 
 const SSDP_SERVICE_TYPE = 'urn:schemas-sony-com:service:IRCC:1';
+//urn:schemas-sony-com:service:ScalarWebAPI:1
 const SERVICE_PROTOCOLS = [
   'accessControl',
   'appControl',
@@ -21,14 +25,27 @@ const SERVICE_PROTOCOLS = [
   'system',
   'videoScreen'
 ];
+
 const DEFAULT_TIME_BETWEEN_COMMANDS = 350;
+const TIMEOUT = (ms) => new Promise((res) => setTimeout(res, ms)); 
 
 class Bravia {
-  constructor(host, port = 80, psk = '0000', timeout = 5000) {
-    this.host = host;
-    this.port = port;
-    this.psk = psk;
-    this.timeout = timeout;
+  constructor(options) {
+  
+    this.host = options.host;
+    this.mac = options.mac;
+    this.port = options.port || 80;
+    this.psk = options.psk;
+    this.timeout = options.timeout * 1000 || 5000;
+    
+    this.authWithPIN = options.pin;
+    if(!this.psk && !this.authWithPIN)
+      this.authWithPIN = true;
+      
+    this.credentials = {
+      token: options.token
+    };
+    
     this.protocols = SERVICE_PROTOCOLS;
     this.delay = DEFAULT_TIME_BETWEEN_COMMANDS;
 
@@ -41,25 +58,23 @@ class Bravia {
     this._codes = [];
   }
 
-  static discover(timeout = 3000) {
-    return new Promise((resolve, reject) => {
-      let ssdp = new SsdpClient();
-      let discovered = [];
-
-      ssdp.on('response', (headers, statusCode, data) => {
-        if (statusCode === 200) {
-          Request.get(headers.LOCATION, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-              parseString(body, (err, result) => {
-                if (!err) {
-                  try {
-                    let device = result.root.device[0];
-                    if (!device.serviceList) {  // Not all devices return a serviceList (e.g. Philips Hue gateway responds without serviceList)
-                      return;
-                    }
+  async discover(timeout) {
+  
+    let ssdp = new SsdpClient();
+    let discovered = [];
+  
+    ssdp.on('response', async (headers, statusCode, data) => {
+      if (statusCode === 200) {
+        try {
+          const response = await axios(headers.LOCATION);
+          if(response.status === 200){
+            parseString(response.data, (err, result) => {
+              if (!err) {
+                try {
+                  let device = result.root.device[0];
+                  if (device.serviceList) {  // Not all devices return a serviceList (e.g. Philips Hue gateway responds without serviceList)
                     let service = device.serviceList[0].service
                       .find(service => service.serviceType[0] === SSDP_SERVICE_TYPE);
-
                     let api = URL.parse(service.controlURL[0]);
                     discovered.push({
                       host: api.host,
@@ -68,139 +83,305 @@ class Bravia {
                       manufacturer: device.manufacturer[0],
                       manufacturerURL: device.manufacturerURL[0],
                       modelName: device.modelName[0],
-                      UDN: device.UDN[0]
+                      UDN: device.UDN[0],
+                      services: device.serviceList[0].service,
+                      scalar: device['av:X_ScalarWebAPI_DeviceInfo'],
+                      ircc: device['av:X_IRCC_DeviceInfo'],
+                      codes: device['av:X_IRCCCodeList']
                     });
-                  } catch(e) {
-                    failed(new Error(`Unexpected or malformed discovery response: ${result}.`));
                   }
-                } else {
-                  failed(new Error(`Failed to parse the discovery response: ${body}.`));
+                } catch(e) {
+                  failed(new Error(`Unexpected or malformed discovery response: ${result}.`));
                 }
-              });
+              } else {
+                failed(new Error(`Failed to parse the discovery response: ${response.data}.`));
+              }
+            });
+          } else {
+            failed(new Error(`Error retrieving the description metadata for device ${data.address}.`));
+          }
+        } catch(error){
+          failed(new Error(`Error retrieving the description metadata for device ${data.address}.`));
+        }
+      }
+    });
+      
+    ssdp.search(SSDP_SERVICE_TYPE);
+
+    let failed = error => {
+      ssdp.stop();
+      throw error;
+    };
+    
+    await TIMEOUT((timeout*1000||5000));
+
+    ssdp.stop();
+    
+    return discovered;
+
+  }
+
+  async getIRCCCodes() {
+  
+    if (this._codes.length > 0) {
+      return this._codes;
+    }
+  
+    this._codes = await this.system.invoke('getRemoteControllerInfo');
+    
+    return this._codes;
+          
+  }
+
+  async send(codes) {
+  
+    if (typeof codes === 'string') {
+      codes = [codes];
+    }
+    
+    for(const code of codes){
+    
+      if (/^[A]{5}[a-zA-Z0-9]{13}[\=]{2}$/.test(code)) {
+        await sendCmd(code);
+      } else {
+        const response = this.getIRCCCodes();
+        let ircc = response.find(ircc => ircc.name === code);
+        if (!ircc) {
+          return new Error(`Unknown IRCC code ${code}.`);
+        }
+        await sendCmd(ircc.value);
+      }
+      
+      TIMEOUT(this.delay);
+    
+    }
+    
+    let sendCmd = async code => {
+    
+      let body = `<?xml version="1.0"?>
+        <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+            <s:Body>
+                <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
+                    <IRCCCode>${code}</IRCCCode>
+                </u:X_SendIRCC>
+            </s:Body>
+        </s:Envelope>`;
+
+      await this._request({
+        path: '/IRCC',
+        body: body
+      });
+      
+      return;
+      
+    };
+    
+    return;
+    
+  }
+  
+  wake(address, options, overWOL){
+  
+    return new Promise((resolve, reject) => {
+      if(!overWOL){
+        this.system
+          .invoke('setPowerStatus', '1.0', { 'status': true })
+          .then(() => {
+            resolve();
+          })
+          .catch(err => {
+            reject(err);
+          });
+      } else {
+        address = address || this.mac;
+        options = options ? options : {};
+        if(/^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$/.test(address)){
+          options = {
+            address: options.address || '255.255.255.255',
+            num_packets: options.num_packets || 10,
+            interval: options.interval || 100
+          };
+          wol.wake(address, options, function(error) {
+            if(error) return reject(error);
+            resolve('Magic packets send to ' + address);
+          });
+        } else {
+          reject(new Error('No valid MAC addresss'));
+        }
+      }
+    });
+  
+  }
+  
+  async pair(user, pin){
+    
+    try { 
+    
+      user = user ? user : {};
+      
+      user.name = user.name || 'Bravia';
+      user.uuid = user.uuid || this.genUUID();
+       
+      const headers = {};
+       
+      if(pin){
+       
+        headers.Authorization = 'Basic ' + base64.encode(':' + pin);
+       
+      } else if(this.token) {
+       
+        headers.Cookie = this.token;
+       
+      }
+        
+      const post_data = `{
+        "id": 8,
+        "method": "actRegister",
+        "version": "1.0",
+        "params": [
+          {
+            "clientid":"${user.name}:${user.uuid}",
+            "nickname":"${user.name}"
+          },
+          [
+            {
+              "clientid":"${user.name}:${user.uuid}",
+              "value":"yes",
+              "nickname":"${user.name}",
+              "function":"WOL"
+            }
+          ]
+        ]
+      }`;  
+       
+      const response = await axios.post(this._url + '/accessControl', post_data, { headers: headers });
+       
+      if(response.headers['set-cookie']){
+    
+        this.credentials = {
+          name: user.name,
+          uuid: user.uuid,
+          token: response.headers['set-cookie'][0].split(';')[0].split('auth=')[1]
+        };   
+    
+        return this.credentials;
+    
+      } else if(response.data && response.data.error && response.data.error.includes(40005)){
+       
+        throw new Error('Please turn on TV to handle authentication process through PIN.');
+       
+      } else {
+       
+        throw response.data;
+       
+      }
+     
+    } catch(error) {
+     
+      if(error.response && error.response.status === 401){
+      
+        const response = await prompt({
+          type: 'input',
+          name: 'pin',
+          message: 'Please enter the four-digit PIN.'
+        });
+        
+        return this.pair(user, response.pin);
+      
+      } else {
+    
+        throw error;
+    
+      }
+     
+    }
+  
+  }
+
+  async _request(opts) {
+  
+    const options = {
+      timeout: this.timeout,
+      url: this._url + opts.path,
+      method: 'post',
+      data: opts.json,
+      headers: {
+        'Content-Type': 'text/xml; charset=UTF-8',
+        'SOAPACTION': '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"'
+      } 
+    };
+  
+    try {
+    
+      if(this.authWithPIN){
+      
+        if(!this.credentials.token)
+          this.credentials = await this.pair();
+      
+        options.headers.Cookie = 'auth=' + this.credentials.token;
+        
+      } else {
+      
+        options.headers['X-Auth-PSK'] = this.psk;
+     
+      }
+      
+      let response = await axios(options);
+      
+      if(response.status !== 200){
+        if(response.data.error){
+          throw new Error(response.data.error[1]);
+        } else {
+          parseString(response.data, (err, result) => {
+            if (!err) {
+              try {
+                throw new Error(result['s:Envelope']['s:Body'][0]['s:Fault'][0]['detail'][0]['UPnPError'][0]['errorDescription'][0]);
+              } catch (e) {
+                throw new Error(`Unexpected or malformed error response: ${result}.`);
+              }
             } else {
-              failed(new Error(`Error retrieving the description metadata for device ${data.address}.`));
+              throw new Error(`Failed to parse the error response: ${response.data}.`);
             }
           });
         }
-      });
-
-      ssdp.search(SSDP_SERVICE_TYPE);
-
-      let failed = error => {
-        ssdp.stop();
-        clearTimeout(timer);
-        reject(error);
-      };
-
-      let timer = setTimeout(() => {
-        ssdp.stop();
-        resolve(discovered);
-      }, timeout);
-    });
-  }
-
-  getIRCCCodes() {
-    return new Promise((resolve, reject) => {
-      if (this._codes.length > 0) {
-        resolve(this._codes);
-        return;
       }
+      
+      if(response.data.error) {
+      
+        if(response.data.error.includes(40005)){ //display off
+          await this.wake();
+          await TIMEOUT(500);
+          response = await this._request(opts);
+        } else {
+        
+          console.log(response.data);
+        
+          throw response.data.error;
+        }
 
-      this.system
-          .invoke('getRemoteControllerInfo')
-          .then(codes => {
-            this._codes = codes;
-            resolve(this._codes);
-          }, reject);
-    });
-  }
-
-  send(codes) {
-    return new Promise((resolve, reject) => {
-      if (typeof codes === 'string') {
-        codes = [codes];
       }
-
-      let index = 0;
-      let next = () => {
-        if (index < codes.length) {
-          let code = codes[index++];
-          if (/^[A]{5}[a-zA-Z0-9]{13}[\=]{2}$/.test(code)) {
-            send(code);
-          } else {
-            this.getIRCCCodes()
-                .then(response => {
-                  let ircc = response.find(ircc => ircc.name === code);
-                  if (!ircc) {
-                    reject(new Error(`Unknown IRCC code ${code}.`));
-                    return;
-                  }
-
-                  send(ircc.value);
-                }, reject);
-          }
-        } else {
-          resolve();
-        }
-      };
-
-      let send = code => {
-        let body = `<?xml version="1.0"?>
-          <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-              <s:Body>
-                  <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
-                      <IRCCCode>${code}</IRCCCode>
-                  </u:X_SendIRCC>
-              </s:Body>
-          </s:Envelope>`;
-
-        this._request({
-          path: '/IRCC',
-          body: body
-        }).then(() => setTimeout(() => next(), this.delay), reject);
-      };
-
-      next();
+      
+      return response;
+    
+    } catch(error) {
+    
+      if (error.response) {
+        throw new Error(`Response error, status code: ${error.response.status}.`);
+      } else {
+        throw error;
+      } 
+    
+    }
+    
+  }
+  
+  genUUID () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0; var v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
-
-  _request(options) {
-    return new Promise((resolve, reject) => {
-      options.timeout = this.timeout;
-      options.url = this._url + options.path;
-      options.headers = {
-        'Content-Type': 'text/xml; charset=UTF-8',
-        'SOAPACTION': '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"',
-        'X-Auth-PSK': this.psk
-      };
-
-      Request.post(options, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          resolve(body);
-        } else {
-          if (error) {
-            reject(error);
-          } else if (response.statusCode != 200){
-            reject(new Error(`Response error, status code: ${response.statusCode}.`))
-          } else if (body.error) {
-            reject(new Error(body.error[1]));
-          } else {
-            parseString(body, (err, result) => {
-              if (!err) {
-                try {
-                  reject(new Error(result['s:Envelope']['s:Body'][0]['s:Fault'][0]['detail'][0]['UPnPError'][0]['errorDescription'][0]));
-                } catch (e) {
-                  reject(new Error(`Unexpected or malformed error response: ${result}.`));
-                }
-              } else {
-                reject(new Error(`Failed to parse the error response: ${body}.`));
-              }
-            });
-          }
-        }
-      });
-    });
-  }
+  
 }
 
 module.exports = Bravia;
