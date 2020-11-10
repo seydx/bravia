74689095a35ce27b9dde36c120aadcafd78005af
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-10 12:21:04 +0100

    Added new options

--- bin/cli.js
+++ bin/cli.js
@@ -31,6 +31,52 @@ cli
       logger.debug(error.stack);
       process.exit();
     }
-  });
+  })
+  
+  .command('discover', 'Discover alls your TVs in network')
+  .option('-t, --timeout <timeout>', 'The amount of time (in seconds) to wait for the response (Default 5s)', cli.INTEGER)
+  .action(async (args, options, logger) => {
+    try {
+      const bravia = new Bravia({
+        timeout: options.timeout || 5
+      });
+      const devices = await bravia.discover(options.timeout || 5);
+      for (let device in devices) {
+        console.log(devices[device]);
+      }
+      process.exit();
+    }
+    catch (error) {
+      logger.error(error.message);
+      logger.debug(error.stack);
+      process.exit();
+    }
+  })
+  
+  .command('methods', 'Retrieves all the system method types and versions')
+  .argument('<host>', 'The address of your Bravia TV', cli.STRING)
+  .option('--psk <PSK>', 'Pre-Shared Key', cli.STRING)
+  .option('--token <Token>', 'Token', cli.STRING)
+  .option('-p, --port <port>', 'The port of your Bravia TV (Default: 80)', cli.INTEGER)
+  .option('-t, --timeout <timeout>', 'The amount of time (in seconds) to wait for the response (Default 5s)', cli.INTEGER)
+  .action(async (args, options, logger) => {
+    try {
+      const bravia = new Bravia({
+        host: args.host,
+        port: options.port || 80,
+        token: options.token || false,
+        psk: options.psk || false,
+        timeout: options.timeout || 5000
+      });
+      const methods = await bravia.getAllMethodTypes();
+      console.log(JSON.stringify(methods, null, 2));
+      process.exit();
+    }
+    catch (error) {
+      logger.error(error.message);
+      logger.debug(error.stack);
+      process.exit();
+    }
+  })
 
 cli.parse(process.argv);
