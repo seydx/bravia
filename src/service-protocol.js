46464cbc0e73146ba15aa3aff14a72a77552bd4d
Seyd <34152761+SeydX@users.noreply.github.com>
2020-12-04 08:41:29 +0100

    Update service-protocol.js

--- src/service-protocol.js
+++ src/service-protocol.js
@@ -9,6 +9,52 @@ class ServiceProtocol {
     this.bravia = bravia;
     this.protocol = protocol;
     this._methods = [];
+
+  }
+  
+  async getVersions() {
+    
+    const versions = await this.invoke('getVersions');
+    return versions;
+    
+  }
+
+  async getMethodTypes(version) {
+   
+    if (this._methods.length > 0) {
+
+      if (version) {
+        
+        let methods = this._methods.filter(method => method.version === version);
+        return methods;
+        
+      } else {
+        
+        return this._methods;
+        
+      }
+   
+    }
+    
+    const versions = await this.getVersions();
+    
+    for(const id of versions){
+      
+      let data = await this.invoke('getMethodTypes', '1.0', id);
+      let resolvedData = this.resolveMethods(data, id);
+      
+      this._methods.push({ 
+        endpoint: this.protocol, 
+        version: id, 
+        availableMethods: resolvedData.availableMethods, 
+        availableParams: resolvedData.availableParams, 
+        methods: resolvedData.methods 
+      });
+      
+    }
+    
+    return this._methods;
+
   }
   
   resolveMethods(data, id){
@@ -71,67 +117,33 @@ class ServiceProtocol {
     
   }
   
-  checkApiMethod(method, version, params){
+  async checkApiMethod(method, version, params){
     
-    let apiMethod = this._methods.find(mthd => mthd.availableMethods.includes(method));
+    if(!this.initialized){
+      this.debug('Method types not initialized yet. Initializing available method types before continue');
+      await this.getMethodTypes();
+      this.initialized = true;
+    }
     
-    if(apiMethod){
+    let apiMethod = this._methods.filter(mthd => mthd.availableMethods.includes(method));
     
-      if(Array.isArray(apiMethod)){
-        let apiMethodByVersion = apiMethod.find(method => method.version === version);
-        if(apiMethodByVersion){
-          apiMethod = apiMethodByVersion;
-        } else {
-          apiMethod = apiMethod[apiMethod.length-1];
-        }
+    if(apiMethod.length){
+    
+      let apiMethodByVersion = apiMethod.find(method => method.version === version);
+      
+      if(apiMethodByVersion){
+        apiMethod = apiMethodByVersion;
+      } else {
+        apiMethod = apiMethod[apiMethod.length-1];
       }
    
-      if(apiMethod.version !== version){
+      if(apiMethod.version !== version)
         this.debug('Requested version (%s) for %s could not be found. Replacing it with v%s', version, method, apiMethod.version);
-      } else {
-        this.debug('Requested version %s for %s found', version, method);
-      }
       
       this.debug('Endpoint options %O', apiMethod);
-      
-      version = apiMethod.version;
-      
-      if(params.length){
-      
-        let paramMethod = apiMethod.methods.find(availableMethod => availableMethod.method === method);
-        
-        this.debug('Method options %O', paramMethod);
-        this.debug('Request params %O', params);
-        
-        if(apiMethod.availableParams[method]){
-          let paramsArray = [];
-          
-          params.forEach(prm => {
-            if(typeof prm === 'string'){
-              paramsArray.push(prm);
-            } else {
-              Object.keys(prm).forEach(prm2 => {
-                paramsArray.push(prm2);
-              });
-            }
-          });
-          
-          let notFoundParams = [];
-          
-          paramsArray.forEach(prm => {
-            if(!apiMethod.availableParams[method].includes(prm)){
-              this.debug('Parameter %s not found for %s (%s)', prm, method, version);
-              notFoundParams.push(prm);
-            }
-          });
-          
-          if(notFoundParams.length){
-            throw new Error('Requested Parameter (' + notFoundParams.toString() + ') could not be found!');
-          }
-            
-        }
-        
-      }
+      this.debug('Method options %O', apiMethod.methods.find(availableMethod => availableMethod.method === method));
+
+      return apiMethod;
    
     } else {
    
@@ -141,86 +153,20 @@ class ServiceProtocol {
     
   }
 
-  async getVersions() {
-    const versions = await this.invokeMethods('getVersions');
-    return versions;
-  }
-
-  async getMethodTypes(version) {
-   
-    if (this._methods.length > 0) {
-    
-      this.initialized = true;
-    
-      if (version) {
-        return this._methods.find(method => method.version === version);
-      } else {
-        return this._methods;
-      }
-   
-    }
-    
-    const versions = await this.getVersions();
-    
-    for(const id of versions){
-      let data = await this.invokeMethods('getMethodTypes', '1.0', id);
-      let resolvedData = this.resolveMethods(data, id);
-      
-      this._methods.push({ 
-        endpoint: this.protocol, 
-        version: id, 
-        availableMethods: resolvedData.availableMethods, 
-        availableParams: resolvedData.availableParams, 
-        methods: resolvedData.methods 
-      });
-    }
-    
-    this.initialized = true;
-    
-    return this._methods;
-
-  }
-
   async invoke(method, version, params, turnOn) {
     
+    let apiMethod;
+    
     params = params ? [params] : [];
     version = version || '1.0';
-  
-    if(!this.initialized){
-      this.debug('Method types not initialized yet. Initializing available method types before continue');
-      await this.getMethodTypes();
+    
+    if(method !== 'getVersions' && method !== 'getMethodTypes'){
+      apiMethod = await this.checkApiMethod(method, version, params);
+      version = apiMethod.version;
     }
-    
-    this.checkApiMethod(method, version, params);
-    
+      
     this.debug('Executing action %s (%s)', method, version);
 
-    const response = await this.bravia._request({
-      path: `/${this.protocol}`,
-      json: {
-        id: 3,
-        method: method,
-        version: version,
-        params: params
-      },
-      turnOn: turnOn
-    });
-
-    if (response.data.results) {
-      return response.data.results;
-    } else if (response.data.result) {
-      return response.data.result[(response.data.result.length > 1 ? 1 : 0)];
-    }
-     
-  }
-  
-  async invokeMethods(method, version, params, turnOn) {
-    
-    params = params ? [params] : [];
-    version = version || '1.0';
-
-    this.debug('Executing action ' + method + ' (' + version + ')');
-    
     const response = await this.bravia._request({
       path: `/${this.protocol}`,
       json: {
