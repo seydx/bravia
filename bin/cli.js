637305d0586d1a61d06d85515a18e17da24288fa
SeydX <seyd55@outlook.de>
2021-06-25 16:32:48 +0200

    v2.0.0-beta.1

--- bin/cli.js
+++ bin/cli.js
@@ -4,14 +4,15 @@ const caporal = require('caporal');
 const Bravia = require('../lib/bravia');
 const project = require('../package.json');
 
-let cli = caporal;
+const SERVICE_PROTOCOLS = require('../lib/protocols');
+const cli = caporal;
 
 cli
   .version(project.version)
   .command('pair', 'Pair with a Bravia TV or refresh existing token')
   .argument('<host>', 'The address of your Bravia TV', cli.STRING)
-  .option('-n, --name <name>', 'Name for the app (Default BraviaTV)', cli.STRING)
   .option('-p, --port <port>', 'The port of your Bravia TV (Default: 80)', cli.INTEGER)
+  .option('-n, --name <name>', 'Name for the app (Default BraviaTV)', cli.STRING)
   .action(async (args, options, logger) => {
     try {
       const bravia = new Bravia({
@@ -64,6 +65,42 @@ cli
 
       const methods = await bravia.describe();
       console.log(JSON.stringify(methods, null, 2));
+
+      process.exit();
+    } catch (error) {
+      logger.error(error.message);
+      logger.debug(error.stack);
+
+      process.exit();
+    }
+  })
+
+  .command('exec', 'Execute API call')
+  .argument('<host>', 'The address of your Bravia TV', cli.STRING)
+  .argument('<protocol>', 'Protocol (Endpoint)', cli.STRING)
+  .argument('<service>', 'API Service', cli.STRING)
+  .argument('[version]', 'API Service Version', cli.STRING)
+  .argument('[command]', 'API Command', cli.STRING)
+  .option('-p, --port <port>', 'The port of your Bravia TV (Default: 80)', cli.INTEGER)
+  .option('-n, --name <name>', 'Name for the app (Default BraviaTV)', cli.STRING)
+  .option('-psk, <psk>', 'Pre-Shared Key (if not set, PIN authentication will be used)', cli.STRING)
+  .action(async (args, options, logger) => {
+    try {
+      if (!SERVICE_PROTOCOLS.includes(args.protocol)) {
+        return logger.error(`Invalid Protocol! Available protocols: ${SERVICE_PROTOCOLS.toString()}`);
+      }
+
+      const command = JSON.parse(args.command || '{}');
+
+      const bravia = new Bravia({
+        name: options.name || '@seydx/bravia',
+        host: args.host,
+        port: options.port || 80,
+        psk: options.psk || false,
+      });
+
+      const response = await bravia.exec(args.protocol, args.service, args.version || '1.0', command);
+      console.log(response.body);
 
       process.exit();
     } catch (error) {
